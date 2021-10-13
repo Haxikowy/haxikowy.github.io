4fb0078e5a3ef37c22d3f2300f0426aaaf989c8a
haxikowy <szymon2407@hotmail.com>
2021-10-13 16:42:23 +0200

    Updates

---
+++ precache-manifest.b9d9d37b842f0f1378a92eab453e9ef5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de3427378e86faf76f97e7d45996dc10",
+    "url": "/index.html"
+  },
+  {
+    "revision": "18234b311cc6fabba3dd",
+    "url": "/static/css/main.9fdf754a.chunk.css"
+  },
+  {
+    "revision": "a4fac21e54f9afef60ca",
+    "url": "/static/js/2.16d88521.chunk.js"
+  },
+  {
+    "revision": "59ddb421a2367d8612f6e280fcb71001",
+    "url": "/static/js/2.16d88521.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "18234b311cc6fabba3dd",
+    "url": "/static/js/main.f28785b3.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  }
+]);
